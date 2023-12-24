56811755dd582252c81a261b23c9b7aa78b80d61
wxmbugu <wambugumacharia35@gmail.com>
2023-12-24 11:06:48 +0300

    small fix

--- library/app/Jobs/ExtensionBookLoanJob.php
+++ library/app/Jobs/ExtensionBookLoanJob.php
@@ -25,7 +25,6 @@ class ExtensionBookLoanJob implements ShouldQueue
         foreach ($loans as $loan) {
             // Check if the extension is not approved and the due date has passed
             if ($loan->extended === 'rejected' || ($loan->due_date < Carbon::now()->timestamp && $loan->status === "borrowed")) {
-
                 $overduePeriodInDays = Carbon::now()->diffInDays(Carbon::createFromTimestamp($loan->due_date));
                 $penaltyAmount = ceil($overduePeriodInDays / 14) * 100;
                 $loan->penalty_amount = $penaltyAmount;
@@ -34,7 +33,7 @@ class ExtensionBookLoanJob implements ShouldQueue
                 $loan->save();
             }
 
-            if ($loan->status === 'returned') {
+            if ($loan->status === 'returned' && $loan->penalty_status === 'penalized') {
                 $loan->penalty_status = 'canceled';
                 $loan->save();
             }
