ffc63674994b10b4a901bbc418b84a6fd3ab49f7
wxmbugu <wambugumacharia35@gmail.com>
2023-12-25 11:20:43 +0300

    fixes

--- library/app/Http/Controllers/BookLoansController.php
+++ library/app/Http/Controllers/BookLoansController.php
@@ -74,7 +74,7 @@ class BookLoansController extends Controller
     {
         $loan = BookLoans::find($id);
         // check if the book is borrowed so a user or admin can't cancel the book if it's already borrowed
-        if (($loan->status === 'borrowed' || $loan->status === 'rejected' || $request->status === 'canceled') && !$request->extended) {
+        if (($loan->status === 'borrowed' || $loan->status === 'rejected' || $request->status === 'canceled') && !$request->extended && !$request->status === 'requested') {
             return response()->json([
                 "error" => "You can't cancel a book that is already borrowed."
             ], 400);
